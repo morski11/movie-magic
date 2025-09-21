8ea428668d441fe7ecdd7040528e63fffd07b24c
Martin Martin <morskijk@gmail.com>
2025-09-21 15:28:59 +0300

    added express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("asd");
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1>HEADING IS REVEALED</h1>");
+});
+
+app.listen(5000, () => { console.log('Server is listening on localhost:5000...') });
