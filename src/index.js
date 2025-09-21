29a528ea880e0048451cbb20f2f7cda1d267f205
Martin Martin <morskijk@gmail.com>
2025-09-21 15:53:56 +0300

    add handlebars and show home page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import handlebars from 'express-handlebars'
 
 const app = express();
+
+//make public folder accessible to everyone
 app.use(express.static('public'));
 
+app.engine("hbs", handlebars.engine());
+app.set("view engine", "hbs");
+
 app.get("/", (req, res) => {
-    res.send("<h1>HEADING IS REVEALED</h1>");
+    res.render('home.hbs');
 });
 
 app.listen(5000, () => { console.log('Server is listening on localhost:5000...') });
