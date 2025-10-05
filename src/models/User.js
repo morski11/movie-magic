70708134e7646713a9467d355576f3f21e6a70cd
Martin Martin <morskijk@gmail.com>
2025-10-05 12:57:38 +0300

    add bcrypt for hashing

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 import { Schema, model } from "mongoose";
-
+import bcrypt from 'bcrypt';
 
 const schema = new Schema({
     email: {
@@ -12,6 +12,18 @@ const schema = new Schema({
     }
 })
 
+
+schema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(11);
+
+    const hashedPass = await bcrypt.hash(this.password, salt);
+    console.log(hashedPass);
+
+    this.password = hashedPass;
+})
+
 const User = model('user', schema);
 
+
 export default User;
+
